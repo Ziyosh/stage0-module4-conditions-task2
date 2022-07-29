9171de013a4454bb7f1020ec42e13fd247f8c7fc
Ziyosh <ziyodaakhmedova1998@gmail.com>
2022-07-29 17:46:30 +0300

    Last 4.2.2 commit!

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -3,18 +3,10 @@ package school.mjc.stage0.conditions.task2;
 public class CoinFlip {
     public static void throwCoin(int from1UpTo1000) {
 
-        from1UpTo1000 = 501;
-
-
         if(from1UpTo1000 >= 500 ){
             System.out.println("Tail");
         }else{
             System.out.println("Eagle");
         }
-
-    }
-
-    public static void main(String[] args){
-        throwCoin(Integer.parseInt("Tail"));
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,21 +2,11 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public static void printGreatest(int first, int second) {
-        first = 12;
-        second = -400;
 
         if(first > second){
             System.out.println(first);
         }else{
             System.out.println(second);
         }
-    }
-
-    public static void main(String[] args){
-        printGreatest(12, -400);
-    }
-
-    private static void printGreatest(int i) {
-
     }
 }
